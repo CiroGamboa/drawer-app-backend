ac9b653f40cf08609717a4d820b12a72fcd8e044
Ciro Gamboa <cirossj10@hotmail.com>
2022-11-07 19:50:19 -0500

    Applied first migration

--- backendApp/draws/models.py
+++ backendApp/draws/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Draw(models.Model):
+    draw_title = models.CharField(max_length=200)
+    draw_payload = models.TextField(max_length=2000)
+
